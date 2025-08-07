ea406edc38eaee8843c1451e7680686ce664450c
Tsquareed014 <t.tschanz14@gmail.com>
2025-08-06 23:13:24 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -3,8 +3,6 @@ import sys
 import os
 import pytest
 
-# Add the root directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 # Run pytest with the specified arguments
-pytest.main(["--maxfail=1", "--disable-warnings", "-q", "tests/"])
+pytest.main(["--maxfail=1", "--disable-warnings", "-q", "test_model_loader.py"])
